98fff6ca1aa9eafd137464fbb6b9c07a9dba23c6
SMRakib-ai <smr385070@gmail.com>
2025-11-06 00:39:59 +0600

    initial commit

---
+++ finallabtask1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>DONOR INFORMATION</title>
+</head>
+<body>
+
+<h1> Welcome to AIUB</h1>
+<p>portal.</p>
+Enter your name : <input type="text"> <br>
+
+department : <input type="radio">  CSR 
+<input type="radio">  deg <br>
+<input type="radio">  BBA<br> 
+ 
+dept : <input type="checkbox">  BBA 
+<input type="checkbox">  cse
+
+</body>
+</html>
